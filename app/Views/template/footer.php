3580575870098d62db8b8b7b4f89d41ec819875f
seven070791 <freddysubero208@gmail.com>
2025-02-24 16:14:39 -0400

    prueba con axios

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -20,6 +20,7 @@
 <script src="<?php echo base_url(); ?>/datatable_responsive/js/dataTables.fixedHeader.min.js"></script>
 <script src="<?php echo base_url(); ?>/datatable_responsive/js/dataTables.responsive.min.js"></script>
 <script src="<?php echo base_url(); ?>/datatable_responsive/js/responsive.bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 
 
