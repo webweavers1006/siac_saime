53e036d898951546ef658e26d6fc4fb2b3083cde
seven070791 <freddysubero208@gmail.com>
2026-01-12 16:07:12 -0400

    CAMBIO DE MAPA

--- public/static/js/map-core.js
+++ public/static/js/map-core.js
@@ -3,9 +3,9 @@
   const initialCenter = [10.4806, -66.9036];
   const initialZoom = 6;
   const map = L.map('map').setView(initialCenter, initialZoom);
-  L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
     crossOrigin: true
   }).addTo(map);
   global.MapApp = global.MapApp || {};
