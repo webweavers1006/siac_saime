dca6a9b41a3138209eb452552ef120a9c532f59b
seven070791 <freddysubero208@gmail.com>
2026-05-21 08:10:38 -0400

    SE COMENTO EL BLOQUE DEL SELECT DE OPERADOR EN REPORTE DE TALLERES PARTICIPANTES

--- app/Views/reportes/talleres_participantes/content.php
+++ app/Views/reportes/talleres_participantes/content.php
@@ -481,14 +481,14 @@
                                 </h3>
                             </div>
                             
-                            <div class="col-md-3">
-                                <div class="d-flex align-items-center justify-content-end">
-                                    <label for="operador" class="m-0 me-2 text-nowrap">Operador &nbsp;&nbsp; </label>
-                                    <select class="form-control" id="operador" name="operador">
-                                        <option value="0" disabled selected>Seleccione</option>
-                                    </select>
-                                </div>
-                            </div>
+                           <div class="col-md-3 d-none">
+                        <div class="d-flex align-items-center justify-content-end">
+                            <label for="operador" class="m-0 me-2 text-nowrap">Operador &nbsp;&nbsp; </label>
+                            <select class="form-control" id="operador" name="operador">
+                                <option value="0" disabled selected>Seleccione</option>
+                            </select>
+                        </div>
+                    </div>
                         </div>
                     </div>
 
